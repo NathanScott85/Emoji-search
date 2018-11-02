36441b622b9ba5a19bc7233995addaf382f6ac7b
nathanscott85 <nathansfeely@gmail.com>
2018-11-02 21:02:29 +0000

    Updates

---
+++ precache-manifest.5c7908fe14f86738643bc1a229757802.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8137670fc83719881711",
+    "url": "/Emoji-search/static/js/runtime~main.8137670f.js"
+  },
+  {
+    "revision": "0ba4d5429be73114d4c5",
+    "url": "/Emoji-search/static/js/main.0ba4d542.chunk.js"
+  },
+  {
+    "revision": "2404498db8409c2df3ad",
+    "url": "/Emoji-search/static/js/1.2404498d.chunk.js"
+  },
+  {
+    "revision": "0ba4d5429be73114d4c5",
+    "url": "/Emoji-search/static/css/main.96688e04.chunk.css"
+  },
+  {
+    "revision": "883e465a539bcbe69e2a49b983fc1594",
+    "url": "/Emoji-search/index.html"
+  }
+];
